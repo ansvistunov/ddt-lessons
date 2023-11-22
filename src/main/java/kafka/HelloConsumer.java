2ff63ea345432a3b4c64cec6b9f2118ab52428b4
Alexey Svistunov <ansvistunov@gmail.com>
2023-11-22 09:32:15 +0300

    add kafka Hello consumer&producer

--- src/main/java/kafka/HelloConsumer.java
+++ src/main/java/kafka/HelloConsumer.java
@@ -1,2 +1,38 @@
-package kafka;public class HelloConsumer {
+package kafka;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Arrays;
+import java.util.Properties;
+public class HelloConsumer {
+    public static void main(String[] args) {
+        String username = args[0];          String password = args[1];
+        String brokers = args[2];           String topicName = args[3];
+        String jaasTemplate = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";";
+        String jaasCfg = String.format(jaasTemplate, username, password);
+        String serializer = StringSerializer.class.getName(); String deserializer = StringDeserializer.class.getName();
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", brokers);         properties.put("group.id", username + "-consumer");
+        properties.put("enable.auto.commit", "true");         properties.put("auto.commit.interval.ms", "1000");
+        properties.put("auto.offset.reset", "earliest");         properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer", deserializer);         properties.put("value.deserializer", deserializer);
+        properties.put("key.serializer", serializer);         properties.put("value.serializer", serializer);
+        properties.put("security.protocol", "SASL_PLAINTEXT");         properties.put("sasl.mechanism", "PLAIN");
+        properties.put("sasl.jaas.config", jaasCfg);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList(topicName));
+        int count = 0;
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
+            if (!records.isEmpty()){
+                for(ConsumerRecord<String, String> record: records) {
+                    count++; System.out.println( String.format("%s: value=%s, key=%s",count,record.value(), record.key()));
+                }
+                //consumer.commitSync();
+            }
+        }
+    }
 }
+
